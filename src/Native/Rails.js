d9b2bd39dd9aee8b4aaab0da3d67dc433c2d931b
Andrew Volozhanin <scarfacedeb@gmail.com>
2016-06-15 13:43:00 +0300

    Rename Native module to make it possible to use it for now
    Blocking: https://github.com/NoRedInk/elm-rails/pull/9

--- src/Native/Rails.js
+++ src/Native/Rails.js
@@ -1,4 +1,4 @@
-var _NoRedInk$elm_rails$Native_Rails = function(){
+var _user$project$Native_Rails = function(){
   var csrfTokenNode = document.head.querySelector('meta[name="csrf-token"]');
   var csrfToken =
       (csrfTokenNode === null || (typeof csrfTokenNode.content !== "string"))
